23274335c7303d1fadd20a0f7dea040468837c0a
huoxudong125 <423huo@163.com>
2017-08-22 23:51:09 +0800

    增加工区进度报表

---
+++ HQF.Daily.Web45/Controllers/WorkAreaProgressesController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using HQF.Daily.Web45.DAL;
+using HQF.Daily.Web45.Models;
+
+namespace HQF.Daily.Web45.Controllers
+{
+    public class WorkAreaProgressesController : Controller
+    {
+        private DailyDbContext db = new DailyDbContext();
+
+        // GET: WorkAreaProgresses
+        public ActionResult Index()
+        {
+            var WorkAreaProgess = db.WorkItemProgresses
+                .Include(t => t.WorkTeam)
+                .Include(t => t.WorkItem)
+                .Include(t => t.WorkItem.WorkArea)
+                .Select(t => new DailyWorkAreaProgress()
+                {
+                    Day = t.CurrentDate,
+                    WorkAreaName = t.WorkItem.WorkArea.Name,
+                    WorkItemName = t.WorkItem.Name,
+                    WorkQuantity = t.WorkQuantity
+                }).ToList(); 
+
+
+
+            return View(WorkAreaProgess);
+        }
+    }
+}
---
+++ HQF.Daily.Web45/Models/WorkAreaProgress.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HQF.Daily.Web45.Models
+{
+    public class WorkAreaProgress
+    {
+        public WorkAreaProgress()
+        {
+            DailyWorkItemProgresses = new List<DaysWorkItemProgress>();
+        }
+        
+        public string WorkAreaName { get; set; }
+
+        
+        List<DaysWorkItemProgress> DailyWorkItemProgresses { get; set; }
+
+      
+
+    }
+
+    public class DaysWorkItemProgress
+    {
+        public DaysWorkItemProgress()
+        {
+            DailyProgesses = new List<DailyWorkProgress>();
+        }
+
+        public string WorkItemName { get; set; }
+
+        public List<DailyWorkProgress> DailyProgesses { get; set; }
+    }
+
+
+    public class DailyWorkProgress
+    {
+
+        public DateTime Day { get; set; }
+
+        public float WorkQuantity { get; set; }
+    }
+
+
+    public class DailyWorkAreaProgress
+    {
+        public DateTime Day { get; set; }
+
+        public string WorkAreaName { get; set; }
+
+        public string WorkItemName { get; set; }
+
+        public double WorkQuantity { get; set; }
+    }
+
+
+}
